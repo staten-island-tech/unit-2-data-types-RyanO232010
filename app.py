c2204169eb121ec1cc33f7744a08491db34610fa
RyanO232010 <ryano92@nycstudents.net>
2025-02-05 09:25:10 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+""" x = 3
+y = float(3)
+print(x,y) """
+#TIPCALCULATOR
+""" def add (x,y):
+    print(x+y)
+
+
+bill = float(input("How much was the bill?"))
+print(type(bill))
+
+tip = int(bill * .15)
+print(add(tip,bill))
+ """
+
