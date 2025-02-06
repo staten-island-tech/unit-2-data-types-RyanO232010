78367a8cd90d757dafc83fded5ed4d69e5ebbc1c
RyanO232010 <ryano92@nycstudents.net>
2025-02-06 09:24:54 -0500

    MadLibs Complete

--- app.py
+++ app.py
@@ -13,3 +13,26 @@ tip = int(bill * .15)
 print(add(tip,bill))
  """
 
+#WORD COUNTER
+""" def count (text) :
+    words = text.split() #split the text into a list of words
+    return len(words) #return the number of words
+response = input("Please enter a sentence or paragraph: ")
+
+word_count= count(response)
+
+print(f"The word count is: {word_count}") """
+
+
+#MADLIBS
+
+""" a = input("Please input a Verb: ")
+b = input("Please input a Second Verb: ")
+c = input("Please input a Noun: ")
+d = input("Please input a Number: ")
+e = input("Please input a Celebrity: ")
+
+print("Mohamed had to",a,",even though the quickest way to class is when you",b,". Mohamed being stupid got chased by a",c,", turns out he was actually being chased by",d,c,". While running like a idiot, he somehow met",e) """
+
+odd_or_even = input("Please input a number: ")
+x = odd_or_even
